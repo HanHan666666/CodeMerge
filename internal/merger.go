6d50a4b0a47fe8ff9bec5c87345b8e0ae6f68d65
han <tar.zip@outlook.com>
2023-07-15 19:11:53 +0800

    :bug:fix: Remove the functionality of merging files starting with "."

--- internal/merger.go
+++ internal/merger.go
@@ -16,7 +16,9 @@ func GetAllFileIncludeSubFolder(folder string) ([]string, error) {
 			return err
 		}
 
-		if fi.IsDir() {
+		if !fi.IsDir() {
+			result = append(result, path)
+		} else {
 			// 如果这个目录是.开头的，说明是一个隐藏目录，忽略这个目录
 			if fi.Name()[0] == '.' {
 				return filepath.SkipDir
